940d2fae66405fbeaec0471b38ae5271229b5074
Boldizsar Szabo <boldizsar.szabo@rabit.hu>
2026-06-05 09:14:53 +0200

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="flex min-h-screen items-center justify-center bg-linear-to-br from-indigo-500 to-purple-700"
+  >
+    <h1 class="text-6xl font-bold text-white drop-shadow-lg">Hello World</h1>
+  </div>
+</template>
+
+<style scoped></style>
